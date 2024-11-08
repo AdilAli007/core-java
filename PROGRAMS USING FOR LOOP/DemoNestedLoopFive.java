b44956d46f301f78c5ba1f3ffc5e8ec8b9bef808
Adil Ali <88463711+AdilAli007@users.noreply.github.com>
2024-11-08 17:51:47 +0500

    Created programs on nested for loop

---
+++ PROGRAMS USING FOR LOOP/DemoNestedLoopFive.java
@@ -0,0 +1,25 @@
+/* 	
+	Required Output
+
+	5 5 5 5 5
+	4 4 4 4
+	3 3 3
+	2 2 
+	1
+
+*/
+
+class DemoNestedLoopFive
+{
+	public static void main(String args[]) 
+   {
+	for(int i=5; i>=1; i--)
+	{
+		for(int j=1; j<=i; j++)
+		{
+			System.out.print(i+" ");
+		}
+			System.out.println();
+	}  
+   }
+}
---
+++ PROGRAMS USING FOR LOOP/DemoNestedLoopThree.java
@@ -0,0 +1,25 @@
+/* 	
+	Required Output
+
+	1 1 1 1 1 
+	2 2 2 2
+	3 3 3
+	4 4 
+	5
+
+*/
+
+class DemoNestedLoopThree
+{
+	public static void main(String args[]) 
+   {
+	for(int i=1; i<=5; i++)
+	{
+		for(int j=i; j<=5; j++)
+		{
+			System.out.print(i+" ");
+		}
+			System.out.println();
+	}  
+   }
+}
